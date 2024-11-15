ba68c2249f6984231b9398dfbc30bf08b79ee9f7
Rohin Bhargava <rohinbharg@gmail.com>
2024-11-14 23:05:29 -0500

    share logic for extracting middleware and handlers

--- packages/core/src/http/router/expressLikeRouter.ts
+++ packages/core/src/http/router/expressLikeRouter.ts
@@ -816,6 +816,52 @@ export class ForklaunchExpressLikeRouter<
     );
   }
 
+  #processTypedHandlerOrMiddleware<
+    Path extends `/${string}`,
+    P extends ParamsObject<SV>,
+    ResBodyMap extends ResponsesObject<SV>,
+    ReqBody extends Body<SV>,
+    ReqQuery extends QueryObject<SV>,
+    ReqHeaders extends HeadersObject<SV>,
+    ResHeaders extends HeadersObject<SV>,
+    LocalsObj extends Record<string, unknown>
+  >(
+    handler:
+      | ContractDetailsOrMiddlewareOrTypedHandler<
+          SV,
+          'middleware',
+          Path,
+          P,
+          ResBodyMap,
+          ReqBody,
+          ReqQuery,
+          ReqHeaders,
+          ResHeaders,
+          LocalsObj
+        >
+      | undefined,
+    middleware: RouterHandler[]
+  ) {
+    if (
+      isTypedHandler<
+        SV,
+        'middleware',
+        Path,
+        P,
+        ResBodyMap,
+        ReqBody,
+        ReqQuery,
+        ReqHeaders,
+        ResHeaders,
+        LocalsObj
+      >(handler)
+    ) {
+      middleware.push(...(handler.handlers as RouterHandler[]));
+    } else if (isExpressLikeSchemaHandler(handler)) {
+      middleware.push(handler as RouterHandler);
+    }
+  }
+
   #extractMiddlewareAsRouterHandlers<
     Path extends `/${string}`,
     P extends ParamsObject<SV>,
@@ -855,30 +901,16 @@ export class ForklaunchExpressLikeRouter<
   ) {
     const middleware: RouterHandler[] = [];
 
-    if (
-      isTypedHandler<
-        SV,
-        'middleware',
-        Path,
-        P,
-        ResBodyMap,
-        ReqBody,
-        ReqQuery,
-        ReqHeaders,
-        ResHeaders,
-        LocalsObj
-      >(contractDetailsOrMiddlewareOrTypedHandler)
-    ) {
-      middleware.push(
-        ...(contractDetailsOrMiddlewareOrTypedHandler.handlers as RouterHandler[])
-      );
-    } else if (
-      isExpressLikeSchemaHandler(contractDetailsOrMiddlewareOrTypedHandler)
-    ) {
-      middleware.push(
-        contractDetailsOrMiddlewareOrTypedHandler as RouterHandler
-      );
-    }
+    this.#processTypedHandlerOrMiddleware<
+      Path,
+      P,
+      ResBodyMap,
+      ReqBody,
+      ReqQuery,
+      ReqHeaders,
+      ResHeaders,
+      LocalsObj
+    >(contractDetailsOrMiddlewareOrTypedHandler, middleware);
 
     middleware.push(
       ...this.#extractMiddlewareFromEnrichedTypedHandlerArray<
@@ -939,8 +971,17 @@ export class ForklaunchExpressLikeRouter<
   ) {
     const middleware: (RouterHandler | Internal)[] = [];
 
-    if (
-      isTypedHandler<
+    this.#processTypedHandlerOrMiddleware<
+      Path,
+      P,
+      ResBodyMap,
+      ReqBody,
+      ReqQuery,
+      ReqHeaders,
+      ResHeaders,
+      LocalsObj
+    >(
+      contractDetailsOrMiddlewareOrTypedHandler as ContractDetailsOrMiddlewareOrTypedHandler<
         SV,
         'middleware',
         Path,
@@ -951,18 +992,11 @@ export class ForklaunchExpressLikeRouter<
         ReqHeaders,
         ResHeaders,
         LocalsObj
-      >(contractDetailsOrMiddlewareOrTypedHandler)
-    ) {
-      middleware.push(
-        ...(contractDetailsOrMiddlewareOrTypedHandler.handlers as RouterHandler[])
-      );
-    } else if (
-      isExpressLikeSchemaHandler(contractDetailsOrMiddlewareOrTypedHandler)
-    ) {
-      middleware.push(
-        contractDetailsOrMiddlewareOrTypedHandler as RouterHandler
-      );
-    } else if (
+      >,
+      middleware as RouterHandler[]
+    );
+
+    if (
       isForklaunchExpressLikeRouter<SV, `/${string}`, RouterHandler, Internal>(
         contractDetailsOrMiddlewareOrTypedHandler
       )
