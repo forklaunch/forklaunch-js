a31dc198ed7ddd502568929b26a69a492a1d9b54
Rohin Bhargava <rohinbharg@gmail.com>
2025-01-02 17:40:49 +0100

    added depcheck command

--- cli/tests/depcheck.sh
+++ cli/tests/depcheck.sh
@@ -2,7 +2,7 @@ mkdir -p output/depcheck
 cd output/depcheck
 
 RUST_BACKTRACE=1 cargo run init application depcheck-test-node-application -d postgresql -v zod -f express -r node -t vitest -s billing -s iam -D "Test service" -A "Rohin Bhargava" -L 'apgl'
-
-cd output/depcheck/depcheck-test-node-application
+RUST_BACKTRACE=1 cargo run depcheck -p depcheck-test-node-application
 
 RUST_BACKTRACE=1 cargo run init application depcheck-test-bun-application -d postgresql -v zod -f express -r bun -t vitest -s billing -s iam -D "Test service" -A "Rohin Bhargava" -L "mit"
+RUST_BACKTRACE=1 cargo run depcheck -p depcheck-test-bun-application
