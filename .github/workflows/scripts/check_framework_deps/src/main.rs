3b68a62a345109c1c9cedb6a767df6e770af8016
Rohin Bhargava <rohinbharg@gmail.com>
2025-03-23 02:50:07 -0400

    fix: attempt to refactor github actions after directory name changes

--- .github/workflows/scripts/check_framework_deps/src/main.rs
+++ .github/workflows/scripts/check_framework_deps/src/main.rs
@@ -1,10 +1,10 @@
 use std::fs;
 use toml;
 
-mod check_framework_deps;
+mod check_blueprint_deps;
 
 fn main() {
-    let topology = fs::read_to_string("../../../../framework/.forklaunch/manifest.toml")
+    let topology = fs::read_to_string("../../../../blueprint/.forklaunch/manifest.toml")
         .expect("Failed to read manifest.toml");
     let projects: Vec<String> = toml::from_str::<toml::Value>(&topology)
         .expect("Failed to parse TOML")
@@ -17,10 +17,10 @@ fn main() {
         .as_array()
         .expect("Expected forklaunch_framework to be an array")
         .iter()
-        .map(|project| format!("../../../../framework/{}", project.as_str().unwrap()))
+        .map(|project| format!("../../../../blueprint/{}", project.as_str().unwrap()))
         .collect();
     let project_refs: Vec<&str> = projects.iter().map(|s| s.as_str()).collect();
-    match check_framework_deps::verify_package_versions(&project_refs) {
+    match check_blueprint_deps::verify_package_versions(&project_refs) {
         Ok(_) => println!("All package versions are correct"),
         Err(e) => println!("Error: {}", e),
     }
