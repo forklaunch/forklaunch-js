3a1b45f0132fe5e2fa632c5fbc76dd073a69c6ed
Rohin Bhargava <rohinbharg@gmail.com>
2025-08-03 19:27:51 -0400

    update sample worker

--- blueprint/sample-worker/sdk.types.ts
+++ blueprint/sample-worker/sdk.types.ts
@@ -1,19 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  SdkClient,
-  UnpackSdkClientInput,
-  ValidSdkClientInput
-} from '@forklaunch/core/http';
-import { sampleWorkerRoutes } from './server';
+import { sampleWorkerSdk } from './server';
 
-type SampleWorkerSdkClientInput = {
-  sampleWorker: typeof sampleWorkerRoutes;
-};
-
-type UnpackedSampleWorkerSdkClientInput =
-  UnpackSdkClientInput<SampleWorkerSdkClientInput>;
-
-type ValidatedSampleWorkerSdkClientInput =
-  ValidSdkClientInput<UnpackedSampleWorkerSdkClientInput>;
-
-export type SampleWorkerSdkClient = SdkClient<SampleWorkerSdkClientInput>;
+export type SampleWorkerSdkClient = typeof sampleWorkerSdk;
--- blueprint/sample-worker/server.ts
+++ blueprint/sample-worker/server.ts
@@ -1,38 +1,44 @@
 import { forklaunchExpress, SchemaValidator } from '@forklaunch/blueprint-core';
 import { getEnvVar } from '@forklaunch/common';
+import { sdkClient } from '@forklaunch/core/http';
 import dotenv from 'dotenv';
-import { SampleWorkerRoutes } from './api/routes/sampleWorker.routes';
+import {
+  sampleWorkerRouter,
+  sampleWorkerSdkRouter
+} from './api/routes/sampleWorker.routes';
 import { createDependencies } from './registrations';
+
 //! bootstrap resources and config
 const envFilePath = getEnvVar('DOTENV_FILE_PATH');
 dotenv.config({ path: envFilePath });
-const { serviceDependencies, tokens } = createDependencies();
-const ci = serviceDependencies.validateConfigSingletons(envFilePath);
+
+export const { ci, tokens } = createDependencies(envFilePath);
+
 //! resolves the openTelemetryCollector from the configuration
 const openTelemetryCollector = ci.resolve(tokens.OpenTelemetryCollector);
+
 //! creates an instance of forklaunchExpress
-const app = forklaunchExpress(SchemaValidator(), openTelemetryCollector);
+const schemaValidator = SchemaValidator();
+const app = forklaunchExpress(schemaValidator, openTelemetryCollector);
+
 //! resolves the protocol, host, port, and version from the configuration
 const protocol = ci.resolve(tokens.PROTOCOL);
 const host = ci.resolve(tokens.HOST);
 const port = ci.resolve(tokens.PORT);
 const version = ci.resolve(tokens.VERSION);
 const docsPath = ci.resolve(tokens.DOCS_PATH);
-//! resolves the necessary services from the configuration
-const sampleWorkerServiceFactory = ci.scopedResolver(
-  tokens.SampleWorkerService
-);
-//! constructs the necessary routes using the appropriate Routes functions
-export const sampleWorkerRoutes = SampleWorkerRoutes(
-  () => ci.createScope(),
-  sampleWorkerServiceFactory,
-  openTelemetryCollector
-);
+
 //! mounts the routes to the app
-app.use(sampleWorkerRoutes);
+app.use(sampleWorkerRouter);
+
 //! starts the server
 app.listen(port, host, () => {
   openTelemetryCollector.info(
     `🎉 SampleWorker Server is running at ${protocol}://${host}:${port} 🎉.\nAn API reference can be accessed at ${protocol}://${host}:${port}/api/${version}${docsPath}`
   );
+});
+
+//! exports the SDK for client usage
+export const sampleWorkerSdk = sdkClient(schemaValidator, {
+  sampleWorker: sampleWorkerSdkRouter
 });
