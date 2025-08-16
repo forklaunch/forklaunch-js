becd199a1240d3eca246aee46e176b729261cc84
srtandon <srtandon1012@gmail.com>
2025-08-15 17:20:34 -0700

    UPDATE removed cd line from test/change_app since files now dump in cwd, TODO trace change_application to where packages are referenced and copied over since packages are now under src/modules

--- cli/tests/change_application.sh
+++ cli/tests/change_application.sh
@@ -7,14 +7,14 @@ cd output/change-application
 
 RUST_BACKTRACE=1 cargo run --release init application change-application-test-node-application -p . -d postgresql -f prettier -l eslint -v zod -F hyper-express -r node -t vitest -m billing-base -m iam-base -D "Test service" -A "Rohin Bhargava" -L 'AGPL-3.0'
 
-cd change-application-test-node-application
+# cd change-application-test-node-application
 
 pnpm install
 pnpm build
 
 cd ..
 
-RUST_BACKTRACE=1 cargo run --release change application -p change-application-test-node-application -N change-application-test-bun-application -f biome -l oxlint -v typebox -F express -r bun -t jest -D "Test service 2" -A "Rohin Bhargava A" -L "MIT" -c
+RUST_BACKTRACE=1 cargo run --release change application -p change-application -N change-application-test-bun-application -f biome -l oxlint -v typebox -F express -r bun -t jest -D "Test service 2" -A "Rohin Bhargava A" -L "MIT" -c
 
 cd change-application-test-bun-application
 
