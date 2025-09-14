984cc8f248a6e525a358954499268ecdbaaa374f
Hemant Parihar <hemant.parihar264@gmail.com>
2025-09-15 01:27:59 +0530

    keeping getEnvVar as original

--- framework/common/src/getEnvVar.ts
+++ framework/common/src/getEnvVar.ts
@@ -1,16 +1,6 @@
-// This is a simple function that returns the value of an environment variable.
-// Returns undefined if the environment variable doesn't exist or if not in a Node.js environment.
+// It casts a potentially undefined value to a string, since it will be validated in order to be bootstrapped.
 
-export function getEnvVar(name: string): string | undefined {
-  if (typeof process !== 'undefined' && process.env) {
-    return process.env[name];
-  }
-  if (typeof import.meta !== 'undefined' && 'env' in import.meta) {
-    const env = (
-      import.meta as unknown as { env: Record<string, string | undefined> }
-    ).env;
-    return env[name];
-  }
-
-  return undefined;
+export function getEnvVar(name: string): string {
+  const value = process.env[name];
+  return value as string;
 }
